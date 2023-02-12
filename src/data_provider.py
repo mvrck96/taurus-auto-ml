eb6b97b05a97fb96babea45c2f48d77516d7cda7
Ivan <maverickmsk1@gmail.com>
2023-02-12 19:35:51 +0300

    data provider class added

---
+++ src/data_provider.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from exceptions import FileTypeNotSupported, TargetNotFound
+
+SUPPORTED_FILE_TYPES = ["csv", "parquet"]
+DATA_SOURCES = {
+    "csv": pd.read_csv,
+    "parquet": pd.read_parquet,
+}
+
+
+class DataProvider:
+    def __init__(self, data_source: str, target: str) -> None:
+        """Класс для валидации и получения файла данных.
+
+        @param data_source[str]: Путь до файла с данными.
+        @param target[str]: Целевая перменная.
+        """
+        self.data_source = data_source
+        self.target = target
+
+        # Validate filetype
+        filetype = self.data_source.split(".")[-1]
+        if filetype not in SUPPORTED_FILE_TYPES:
+            raise FileTypeNotSupported(filetype, SUPPORTED_FILE_TYPES)
+
+        # Validate file
+        self.df = DATA_SOURCES[filetype](data_source)
+        df_columns = list(self.df.columns)
+
+        # Validate target
+        if self.target not in df_columns:
+            raise TargetNotFound(self.target, df_columns)
+
+    def get_data(self) -> pd.DataFrame:
+        """Возвращает считанный в pd.DataFrame файл с данными."""
+        return self.df
